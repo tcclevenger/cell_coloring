b4b32a9b61fa19d2c7f4ee8cadaf11c4eb3b0ee3
tcclevenger <tcleven@clemson.edu>
2019-09-04 09:59:42 -0400

    update

--- cell_coloring.cc
+++ cell_coloring.cc
@@ -75,8 +75,6 @@ get_integer_coords (const CellId cell_id)
   else
     global_coord(1) = 1;
 
-  std::cout << "global: (" << global_coord(0) << ", " << global_coord(1) << ")" << std::endl;
-
 
   unsigned int level=1;
   for (auto c : child_indices)
